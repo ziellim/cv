c978a3b396d8c29521081d6fbc30ce3041b5fa9c
Zied Guellim <guellim.z@sfeir.com>
2019-03-17 21:12:06 +0100

    install materiel angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatBadgeModule, MatIconModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
